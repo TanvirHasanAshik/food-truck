e764451278523a38ec19beab3825a9b79397c299
TanvirHasanAshik <tanvirashik41@gmail.com>
2025-01-18 22:08:33 +0600

    env file added

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBg2nUalmXVfyhnueEma5svb-hD9YDo3P8",
-    authDomain: "food-truck-98e12.firebaseapp.com",
-    projectId: "food-truck-98e12",
-    storageBucket: "food-truck-98e12.firebasestorage.app",
-    messagingSenderId: "396539181146",
-    appId: "1:396539181146:web:2cb7cf6661dc6074528063"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId
 };
 
 
